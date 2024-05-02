e55f03c65419bfe2d7b18f0c843342d9d72f80af
mikegyamfi <codeage20@gmail.com>
2024-05-02 16:30:50 +0000

    initial commit

--- business_proj/settings.py
+++ business_proj/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "plankton-app-s46li.ondigitalocean.app",
-    'https://api-u5n23.ondigitalocean.app/',
+    'api-u5n23.ondigitalocean.app',
     'api.cloudhubgh.com',
     'localhost',
     '127.0.0.1'
